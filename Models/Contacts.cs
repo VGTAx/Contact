e5c82c6a16626f24076bc1e01be9ee5f5c4de1a9
VGTAx <102552759+VGTAx@users.noreply.github.com>
2022-09-20 02:31:42 +0300

    Add files via upload

---
+++ Models/Contacts.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Contact.Models
+{
+    public class Contacts
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+        public string JobTitle { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime BirthDate { get; set; } = DateTime.Now;
+    }
+}
