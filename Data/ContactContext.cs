5f8715806f10cffd4e63669c0f7792c15abd45af
VGTAx <102552759+VGTAx@users.noreply.github.com>
2022-09-20 02:36:23 +0300

    Add files via upload

---
+++ Data/ContactContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Contact.Models;
+
+namespace Contact.Data
+{
+    public class ContactContext : DbContext
+    {
+        public ContactContext (DbContextOptions<ContactContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Contact.Models.Contacts> Contact { get; set; } = default!;
+    }
+}
